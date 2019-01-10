f5af4a560aa374aa75ec9ceb85ba8bfbf24a71c7
hans <hello@hans.digital>
2019-01-09 18:02:12 -0800

    implement caching for client-side _app getInitialProps

--- pages/_app.js
+++ pages/_app.js
@@ -4,20 +4,31 @@ import fetch from 'isomorphic-unfetch'
 
 import Sidebar from '../components/sidebar'
 import Nav from '../components/nav'
+import cachedFetch, { overrideCache } from '../lib/cached_json_fetch'
+
+const APP_PROPS = process.env.SERVER_URL + '/api/btc/latest'
 
 export default class RShake extends App {
   static async getInitialProps ({ Component, router, ctx }) {
-    let query = await fetch(process.env.SERVER_URL + '/api/btc/latest')
-    let latest = await query.json()
-
-    let pageProps
-    let sideBarProps = {latest: latest}
-
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx)
+    try {
+      let latest = await cachedFetch(APP_PROPS)
+      const isServerRendered = !!ctx.req
+      let sideBarProps = { latest, isServerRendered }
+      let pageProps
+      if (Component.getInitialProps) {
+        pageProps = await Component.getInitialProps(ctx)
+      }
+      let allProps = Object.assign({}, pageProps, sideBarProps)
+      return { allProps }
+    } catch (err) {
+      throw err
     }
-    let allProps = Object.assign({}, pageProps, sideBarProps)
-    return { allProps }
+  }
+
+  componentDidMount () {
+    if (this.props.isServerRendered) {
+      overrideCache(APP_PROPS, this.props.latest)
+    }
   }
 
   render () {
